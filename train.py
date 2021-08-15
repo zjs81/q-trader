fddd628e10060a04357f277f7f967dfef3d2ea73
zjs81 <30362347+zjs81@users.noreply.github.com>
2021-08-15 00:02:56 -0700

    Update train.py

--- train.py
+++ train.py
@@ -1,9 +1,9 @@
 from agent.agent import Agent
 from functions import *
 import sys
-
+best = {}
 if len(sys.argv) != 4:
-	print "Usage: python train.py [stock] [window] [episodes]"
+	print("Usage: python train.py [stock] [window] [episodes]")
 	exit()
 
 stock_name, window_size, episode_count = sys.argv[1], int(sys.argv[2]), int(sys.argv[3])
@@ -12,42 +12,56 @@ agent = Agent(window_size)
 data = getStockDataVec(stock_name)
 l = len(data) - 1
 batch_size = 32
+batch_size = 64
 
-for e in xrange(episode_count + 1):
-	print "Episode " + str(e) + "/" + str(episode_count)
+for e in range(episode_count + 1):
+	print ("Episode " + str(e) + "/" + str(episode_count))
 	state = getState(data, 0, window_size + 1)
 
 	total_profit = 0
 	agent.inventory = []
+	b=0
+	a=0
 
-	for t in xrange(l):
+	for t in range(l):
 		action = agent.act(state)
 
 		# sit
 		next_state = getState(data, t + 1, window_size + 1)
 		reward = 0
+		a+=1
 
 		if action == 1: # buy
 			agent.inventory.append(data[t])
-			print "Buy: " + formatPrice(data[t])
+			print ("Buy: " + formatPrice(data[t]))
+			b=b+1
+			a=0
 
 		elif action == 2 and len(agent.inventory) > 0: # sell
 			bought_price = agent.inventory.pop(0)
 			reward = max(data[t] - bought_price, 0)
+			reward =(data[t] - bought_price)*100
 			total_profit += data[t] - bought_price
-			print "Sell: " + formatPrice(data[t]) + " | Profit: " + formatPrice(data[t] - bought_price)
+			b=0
+			a=0
+			print ("Sell: " + formatPrice(data[t]) + " | Profit: " + formatPrice(data[t] - bought_price))
 
+		if(b>10 or a>20):
+			reward+=-200
 		done = True if t == l - 1 else False
+
 		agent.memory.append((state, action, reward, next_state, done))
 		state = next_state
+		z = 0
+		#if z % 10 == 0:
+		#	agent.model.save("models/model_ep" + str(e))
+	print("Total Profit For Epoc is " + str(total_profit))
+	best[e] = total_profit
+	
 
-		if done:
-			print "--------------------------------"
-			print "Total Profit: " + formatPrice(total_profit)
-			print "--------------------------------"
-
-		if len(agent.memory) > batch_size:
-			agent.expReplay(batch_size)
-
-	if e % 10 == 0:
-		agent.model.save("models/model_ep" + str(e))
+  
+Keymax = max(best, key=best.get)
+print(Keymax)
+print("The best ep was " + str(Keymax) + ":" + str(best[Keymax]))
+#print(best)
+	
